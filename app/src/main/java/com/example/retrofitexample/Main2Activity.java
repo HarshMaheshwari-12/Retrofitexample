451a26ede266ff391ff08d2f548806c8fcaa961c
derek945 <harsh1429213005@gmail.com>
2019-09-02 17:31:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retrofitexample/Main2Activity.java
@@ -0,0 +1,66 @@
+package com.example.retrofitexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class Main2Activity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<UserListResponse> userListResponseData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        getUserListData(); // call a method in which we have implement our GET type web API
+    }
+
+    private void getUserListData() {
+        // display a progress dialog
+        final ProgressDialog progressDialog = new ProgressDialog(Main2Activity.this);
+        progressDialog.setCancelable(false); // set cancelable to false
+        progressDialog.setMessage("Please Wait"); // set message
+        progressDialog.show(); // show progress dialog
+
+        // Api is a class in which we define a method getClient() that returns the API Interface class object
+        // getUsersList() is a method in API Interface class, in this method we define our API sub url
+        Api.getClient().getUsersList(new Callback<List<UserListResponse>>() {
+            @Override
+            public void success(List<UserListResponse> userListResponses, Response response) {
+                // in this method we will get the response from API
+                progressDialog.dismiss(); //dismiss progress dialog
+                userListResponseData = userListResponses;
+                setDataInRecyclerView(); // call this method to set the data in adapter
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                // if error occurs in network transaction then we can get the error in this method.
+                Toast.makeText(Main2Activity.this, error.toString(), Toast.LENGTH_LONG).show();
+                progressDialog.dismiss(); //dismiss progress dialog
+
+            }
+        });
+    }
+
+    private void setDataInRecyclerView() {
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(Main2Activity.this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        // call the constructor of UsersAdapter to send the reference and data to Adapter
+        UsersAdapter usersAdapter = new UsersAdapter(Main2Activity.this, userListResponseData);
+        recyclerView.setAdapter(usersAdapter); // set the Adapter to RecyclerView
+    }
+}
---
+++ app/src/main/java/com/example/retrofitexample/SamplePojo.java
@@ -0,0 +1,15 @@
+package com.example.retrofitexample;
+
+public class SamplePojo {
+
+    private int userId;
+    private String name;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
